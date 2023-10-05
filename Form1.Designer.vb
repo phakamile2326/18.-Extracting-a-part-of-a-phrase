aca05dc8955a15470f37e9a7c78b4647068d6da5
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-05 02:50:28 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,146 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        btnOk = New Button()
+        Label1 = New Label()
+        NotifyIcon1 = New NotifyIcon(components)
+        TextBox1 = New TextBox()
+        Label2 = New Label()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox3 = New TextBox()
+        TextBox5 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnOk
+        ' 
+        btnOk.Location = New Point(343, 294)
+        btnOk.Name = "btnOk"
+        btnOk.Size = New Size(75, 23)
+        btnOk.TabIndex = 0
+        btnOk.Text = "Ok"
+        btnOk.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(206, 71)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(81, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Enter a Phrase"
+        ' 
+        ' NotifyIcon1
+        ' 
+        NotifyIcon1.Text = "NotifyIcon1"
+        NotifyIcon1.Visible = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(411, 63)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(272, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(206, 113)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(144, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "Enter the Starting Position"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(411, 105)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 4
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(206, 170)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(175, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "Number of characters to extract"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(207, 220)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(80, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "Text extracted"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(411, 162)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 4
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(411, 220)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(100, 23)
+        TextBox5.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label1)
+        Controls.Add(btnOk)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnOk As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackColor = Color.Aquamarine
+    End Sub
+
+    Private Sub btnOk_Click(sender As Object, e As EventArgs) Handles btnOk.Click
+        Dim MyPhrase As String
+        Dim pos, n As Integer
+        MyPhrase = TextBox1.Text
+        pos = TextBox3.Text
+        n = TextBox3.Text
+        TextBox5.Text = Mid(MyPhrase, pos, n)
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
